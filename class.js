1c262b925b501eb306b137d677299f4c58e7e6ae
JiwontwoPunch <80305292+Jiwoncoding@users.noreply.github.com>
2022-03-03 19:45:44 +0900

    Add files via upload
    캐릭터가 키눌림 딜레이없이 부드럽게 이동하도록 만들기 - requestAnimationFrame() 사용

--- class.js
+++ class.js
@@ -2,14 +2,21 @@
 class Hero {
 	constructor(el) {
         this.el = document.querySelector(el);
+        this.movex = 0;   // 히어로가 이동할 거리
+        this.speed = 16;  // 히어로의 스피드
 	}
 	keyMotion() {
         if(key.keyDown['left']) {
             this.el.classList.add('run');
             this.el.classList.add('flip');
+
+            this.movex = this.movex - this.spped;
+
         } else if(key.keyDown['rignt']){
             this.el.classList.add('run');
             this.el.classList.remove('flip');
+
+            this.movex = this.movex + this.spped;
         }
         if(key.keyDown['attack']) {
             this.el.classList.add('attack');
@@ -20,5 +27,8 @@ class Hero {
         if(!key.keyDown['attack']) {
             this.el.classList.remove('attack');
         }
+        
+        this.el.parentNode.style.transform = `translateX(${this.movex}px)`;
+        // 키눌림 딜레이 차이로 캐릭터의 움직임이 자연스럽지 못함.
 	}
 }
--- game.js
+++ game.js
@@ -8,16 +8,21 @@ const key = {
     }
 }
 
+const renderGame = () => {
+    // 재귀함수
+    // 초당 약 60프레임을 그리는 무한반복! 키 눌림의 딜레이없이 캐릭터가 부드럽게 잘 움직임
+    hero.keyMotion();
+    window.requestAnimationFrame(renderGame);
+}
+
 const windowEvent = () => {
     window.addEventListener('keydown', e=> {
         // console.log(key.keyValue[e.which]);
         // keyDown의 키값은 keyValue의 value 값으로
         key.keyDown[key.keyValue[e.keyCode]] = true;
-        hero.keyMotion();
     });
     window.addEventListener('keyup', e=> {
         key.keyDown[key.keyValue[e.which]] = false;
-        hero.keyMotion();
     });
 }
 
@@ -36,6 +41,7 @@ const init = () => {
     hero = new Hero('.hero');
     loadImg();
     windowEvent();
+    renderGame();
 }
 
 // 모든 요소 로드 후 게임 실행 → onload 이벤트 사용
